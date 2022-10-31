f7ab487e903b56433a028d5e91d6ff7294a28d1d
906bc <57206558+906bc906@users.noreply.github.com>
2022-10-31 20:27:11 +0900

    로컬 ical 생성 구현

---
+++ app.js
@@ -0,0 +1,71 @@
+const ical = require('ical-generator');
+const fs = require('fs/promises');
+const axios = require('axios');
+const { DateTime } = require('luxon');
+
+
+async function generateICal() {
+  const cal = initICal();
+  const list = await _fetchAnime();
+  attachEvents(cal, list);
+  
+  await fs.mkdir("./output", {recursive: true});
+  await cal.save('./output/anissia-icalendar.ics')
+}
+
+function initICal() {
+  return ical({
+    name: "anissia-icalendar",
+    prodId: "//906bc906//anissia-icalendar//KR",
+    timezone: "Asia/Seoul",
+  })
+}
+
+async function fetchAnime(){
+  //https://github.com/anissia-net/document/blob/main/api_anime_schdule.md
+  const resultPromises = [];
+  for (let i=0; i<=6; i++){
+    resultPromises.push(axios.get(`https://anissia.net/api/anime/schedule/${i}`))
+  }
+  const resultRawRes = await Promise.all(resultPromises)
+  return resultRawRes.map((day) => day.data);
+}
+
+async function _fetchAnime(){
+  console.log("WARN: You are using mock API fetch");
+  return JSON.parse(await fs.readFile("./saved-list"));
+}
+
+function attachEvents(cal, list) {
+  const dtNow = DateTime.now().setZone('Asia/Seoul');
+  for (let daydiff = 0; daydiff < 7; daydiff++) {
+    const dtTarget = dtNow.plus({day: daydiff});
+    const dtTargetObj = dtTarget.toObject();
+    list[dtTarget.weekday % 7].forEach((anime) => {
+      if (anime.status === "ON"
+      && DateTime.fromISO(anime.startDate).setZone('Asia/Seoul') <= dtTarget
+      && (anime.endDate === "" || DateTime.fromISO(anime.endDate).setZone('Asia/Seoul') >= dtTarget)) {
+        const dtStart = DateTime.fromObject({
+          ...dtTargetObj,
+          hour: parseInt(anime.time.substring(0, 2)),
+          minute: parseInt(anime.time.substring(3, 5)),
+          second: 0,
+          millisecond: 0
+        }).setZone('Asia/Seoul');
+
+        cal.createEvent({
+          start: dtStart,
+          end: dtStart.plus({minute: 30}),
+          summary: anime.subject,
+        })
+      }
+    })
+  }
+}
+
+
+
+//------------
+(
+  async()=>await generateICal()
+)();
